350f4c94d68d395dbc852796d3d9331593eee549
Jungmin Moon <jmmoon96@gmail.com>
2023-12-07 16:26:52 -0500

    Setup files for the main menu and class to act as the interpreter.

---
+++ Main_Menu.java
@@ -0,0 +1,21 @@
+import java.util.*;
+ class Main_Menu {
+    //Dicts for what is considered a valid digit, Letter, nonzerodigit
+    //x = 1 allowed
+    //x = 001 is not allowed
+    //can't -0 that isn't valid
+
+    public static void start() {
+        System.out.println("Welcome to the toy interpreter.");
+        System.out.println("This interpreter will be able to do very basic things.");
+        System.out.println("Type in end to stop the interpreter.");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        do {
+
+        } while(!input.equalsIgnoreCase("end"));
+    }
+    public static void main(String[] args) {
+        start();
+    }
+}
---
+++ Toy_Interpreter.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Toy_Interpreter {
+    //create Dicts here of valid inputs
+    //find a way to interpret various +,-,/,* dependant on what is being passed
+    //each input is taken into an List and stored to check validity
+
+}
