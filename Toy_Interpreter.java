6c3bac51f744a1dad5cbb2f737657d4bc86c0a1c
Jungmin Moon <jmmoon96@gmail.com>
2023-12-07 17:08:32 -0500

    Created Lists very inefficiently for what constitutes as a valid character, nonzerodigit, and a valid Digit with no leading zero.

--- Toy_Interpreter.java
+++ Toy_Interpreter.java
@@ -1,7 +1,19 @@
+import java.lang.reflect.Array;
 import java.util.*;
 public class Toy_Interpreter {
     //create Dicts here of valid inputs
     //find a way to interpret various +,-,/,* dependant on what is being passed
     //each input is taken into an List and stored to check validity
+    String input;
+    List<String> validLetters = Arrays.asList("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
+                                            "q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G",
+                                            "H","I","J","K","L","M","N","O","P",
+                                            "Q","R","S","T","U","V","W","X","Y","Z");
+    List<Integer> validInts = Arrays.asList(1,2,3,4,5,6,7,8,9);
+    List<Integer> validDigits = Arrays.asList(0,1,2,3,4,5,6,7,8,9);
+    Toy_Interpreter(String input) {
+        this.input = input;
+    }
+
 
 }
